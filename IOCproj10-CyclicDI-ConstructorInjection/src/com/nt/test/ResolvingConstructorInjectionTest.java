ff1fa4a72c13345c7d6d2a16dfaa6574d0a1f052
lenovo <lenovo@LAPTOP-T4CEVALM>
2020-07-13 21:57:12 +0530

    CyclicDI ConstructorInjection

---
+++ IOCproj10-CyclicDI-ConstructorInjection/src/com/nt/test/ResolvingConstructorInjectionTest.java
@@ -0,0 +1,21 @@
+package com.nt.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+import com.nt.beans.A;
+
+public class ResolvingConstructorInjectionTest {
+	
+	public static void main(String[] args) {
+		BeanFactory factory = null;
+		A a1 = null;
+		//Create BeanFactory [IoC] container
+		factory = new XmlBeanFactory(new ClassPathResource("com/nt/cfgs/applicationContext.xml"));
+		//get Beans class object
+		a1 = factory.getBean("a1", A.class);
+		System.out.println(a1);
+	} 
+
+}
